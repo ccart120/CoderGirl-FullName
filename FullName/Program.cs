a8e01c71ec8b5c3d3f5a8cb0a4d47da35f85181f
ccartagena879 <ccartagena879@gmail.com>
2019-03-06 17:21:57 -0600

    completed and passed tests

--- FullName/Program.cs
+++ FullName/Program.cs
@@ -4,17 +4,39 @@ namespace FullName
 {
     public static class Program
     {
+
+
         public static void Main()
+
+
+
         {
+            string fName = "";
+            string lName = "";
+            
             // TODO: Ask the user for their first name and then their last name.
-
+            Console.WriteLine("Enter your first name: ");
+            fName = (Console.ReadLine());
+            Console.WriteLine("Enter your last name: ");
+            lName = (Console.ReadLine());
+            
             // TODO: Call a method called FullName that returns their full name.
-
+            string wholeName = FullName(fName, lName);
+            
             // TODO: Output their full name.
-
+            
             Console.ReadLine();
+            Console.WriteLine(wholeName);
         }
-
+        
         // TODO: Create a method call FullName that takes the firstName and lastName as parameters.
+
+        public static string FullName(string fName , string lName)
+        {
+            return (fName + " " + lName);
+
+        }
+        
     }
+        
 }
